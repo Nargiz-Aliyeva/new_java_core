2b952f943c7d436d4b5b51317c639ded320ed108
Nargiz-Aliyeva <nargizaliyeva0120@gmail.com>
2025-08-29 10:35:31 +0400

    practice2

--- polymorphism/src/Main.java
+++ polymorphism/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Test");
+        System.out.printf("dubl");
+        System.out.printf("dubl2");
     }
 }
