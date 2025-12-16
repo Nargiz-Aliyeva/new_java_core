1ab3ec42483f8f4d216ffdacc0e98a14d627a55a
Nargiz Aliyeva <140622785+Nargiz-Aliyeva@users.noreply.github.com>
2025-12-16 17:16:25 +0400

    Add files via upload

---
+++ Java-Core-2/Map/Loop/src/Main.java
@@ -0,0 +1,301 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+//      import java.util.Scanner;
+//
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//        public class Main {
+//            public static void main(String[] args) {
+//loops , iteration , dovr
+//        int a = 1;
+//        while(a<=10){
+//            System.out.println(a);
+//        int a=1;
+//        while (a<=3){
+//            System.out.println(a);
+//          a++;
+//        int z= 1;
+//        while (z<=1444){
+//            System.out.println(z);
+//            z++;
+//      }
+//        int g = 3;
+//        while (g<1000){
+//            System.out.println(g);
+//            g++;
+//        }
+//         int a =0;
+//while (a<100){
+//    System.out.println(a+=2);
+//    int a =100;
+//while (a>=1){
+//    System.out.println(a);
+//    a--;
+//        int c = 300;
+//        while (c >= 1) {
+//            System.out.println(c);
+//            c--;
+//        }
+//        int d=6000;
+//while (d>=1){
+//    System.out.println(d);
+//    d--;
+//}
+
+//        int i = 1; ;
+//        int j = 5;
+//        while (i<10&& j<10){
+//            System.out.println(
+//                    "i= "+i+"+"+ "j="+j
+//
+//            );i++;
+//            j++;
+
+//for(int m =1; m<=10;m++){
+//        System.out.println(m);
+//    }
+//        for (int a =10;a>=1;a--){
+//            System.out.println(a);
+//
+//        }
+//        int q = 100;
+//if(q%2==0){
+//    System.out.println("cwt");
+
+//}
+//else{
+//    System.out.println("tek");
+//}
+//
+//for(int m=1;m<=10;m++){
+//    if(m%2==0){
+//        System.out.println(m+"cwt");
+//    }
+//    else{
+//        System.out.println("tek");
+//    }
+
+//  Scanner scanner = new Scanner(System.in);
+//int b= scanner.nextInt();
+//for(int i=1;i<=10;i++){
+//    System.out.println(b+"*"+i+"="+b*i);
+//}
+//        Scanner scanner = new Scanner(System.in);
+//int s = scanner.nextInt();
+//for (int a =1; a<=12;a++){
+//
+//    System.out.println(a+"*"+s+"="+a*s);
+//
+//
+// }
+
+//for(int a=1;a<=10;a++){
+//    for(int b=1;b<=10;b++){
+//        System.out.println(a+"."+b);
+//
+//    }
+//}
+
+//        for(int a =3;a>=1;a--){
+//            for (int b=1;b<=3;b++){
+//                System.out.println("a="+a+",b="+b);
+//            }
+//        }
+
+//        for(int a=1;a<+50;a++){
+//            if(a%3==0&& a%5==0){
+//                System.out.println(a);
+//            }
+//        }
+//        int sum=0;
+//        for(int a =1;a<=10;a++){
+//            sum+=a;
+//        }
+
+//int sum=0;
+//        for (int i = 1; i <=72; i++) {
+//            sum+=i;
+//
+//        }
+//        System.out.println(sum);
+
+                //for (int i = 1; i <=10 ; i++) {
+                //  if(i==5){break;}
+//            System.out.println(i);
+
+                //}
+//                for (int i = 0; i <=12; i++) {
+//                    if(i==3){
+//                        continue;
+//                    }
+//                    System.out.println(i);
+//
+//                }
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        2
+
+
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+//        int a =1;
+//        while (a<=10){
+//            System.out.println(a);
+//            a++;
+//        }
+//        int a=3;
+//        while (a<=10){
+//            System.out.println(a); a++;
+//        }
+//        int a =10;
+//        while (a>=1  ){
+//            System.out.println(a);a--;
+//        }
+
+    }
+}
---
+++ Java-Core-2/Map/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+//        Human human = new Human(1,"Nergiz");
+//        Human human2 = new Human(2,"Aytac");
+//        Human human3 = new Human(3,"Huseyn");
+//
+//        Map<Integer , String>humanList = new HashMap<>();
+//        humanList.put(human.getId(), human.getName());
+//
+//        System.out.println(humanList);
+
+    }
+
+}
